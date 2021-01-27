378fb9caa03b737a7fbbe1bc53a44519540421d2
andrea <cutillasandrea@gmail.com>
2021-01-27 20:31:02 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -24,7 +24,16 @@
                     <li><a class="btn-store" href="store.html">STORE</a></li>
                     <li><a class="btn-about" href="about.html">ABOUT</a></li>
                     <li><a class="btn-cart" href="cart.html">CART</a></li>
-                    <li><a class="open-button" onclick="openForm()" href="#">
+                    <li>
+                        <?php
+                            session_start();
+                            if(!isset($_SESSION['email'])){
+                                echo '<a class="open-button" href="profile.html">'; 
+                            }
+                            else {
+                                echo '<a class="open-button" onclick="openForm()">';
+                            }
+                        ?>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8 0C9.06087 0 10.0783 0.421427 10.8284 1.17157C11.5786 1.92172 12 2.93913 12 4C12 5.06087 11.5786 6.07828 10.8284 6.82843C10.0783 7.57857 9.06087 8 8 8C6.93913 8 5.92172 7.57857 5.17157 6.82843C4.42143 6.07828 4 5.06087 4 4C4 2.93913 4.42143 1.92172 5.17157 1.17157C5.92172 0.421427 6.93913 0 8 0V0ZM8 10C12.42 10 16 11.79 16 14V16H0V14C0 11.79 3.58 10 8 10Z" fill="black"/>
                         </svg>
@@ -39,7 +48,7 @@
                 </div>
                 <h4>Sign in to your Account</h4>
                 <h6>New to MoMA Design Store?</h6>
-                <button  type="submit" class="btn_simple">Create an account</button>
+                <button  type="submit" class="btn_simple" onclick="window.location.href='create_account.html'">Create an account</button>
 
                 <div id="left_itm">
                     <label for="email">Email</label>
