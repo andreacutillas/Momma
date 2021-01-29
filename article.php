84affee773eb276e7c42af5ac7fb1a3d4d1ff110
Marina <mirinamun@gmail.com>
2021-01-29 12:40:31 +0100

    Menu underline y detalles

--- article.php
+++ article.php
@@ -22,6 +22,34 @@
     <body>
 
         <header>
+
+        <!-- Dropdown -->
+
+        <div class="dropdown" style="float:left;">
+                <button class="dropbtn">
+                    <figure><img class="drop_logo" src="img/dropdown.svg" alt="dropdown"></img></figure>
+                </button>
+                <div class="dropdown-content" style="left:0;">
+                    <a href="index.php"><img src="img/logo.svg"></img></a>
+                    <a href="store.php">STORE</a>
+                    <a href="about.php">ABOUT</a>
+                    <a href="cart.php">CART</a>
+                    <?php
+                                session_start();
+                                if(isset($_SESSION['nombre'])){
+                                    echo '<a class="open-button" href="profile.php">'; 
+                                }
+                                else {
+                                    echo '<a class="open-button" onclick="openForm()">';
+                                }
+                            ?>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8 0C9.06087 0 10.0783 0.421427 10.8284 1.17157C11.5786 1.92172 12 2.93913 12 4C12 5.06087 11.5786 6.07828 10.8284 6.82843C10.0783 7.57857 9.06087 8 8 8C6.93913 8 5.92172 7.57857 5.17157 6.82843C4.42143 6.07828 4 5.06087 4 4C4 2.93913 4.42143 1.92172 5.17157 1.17157C5.92172 0.421427 6.93913 0 8 0V0ZM8 10C12.42 10 16 11.79 16 14V16H0V14C0 11.79 3.58 10 8 10Z" fill="black"/>
+                            </svg>
+                        </a>
+                </div>
+            </div>
+            
             <!-- Menu -->
             <div id="menu">
                 <a class="logo" href="index.php"><img class="logo" src="img/Logo.svg" alt="logo"></a>
